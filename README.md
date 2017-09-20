680915744f445486094546816fc34457b814a115
colanicy <307106874@qq.com>
2017-09-20 16:37:06 +0800

    gesture

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
-# face-compare
-
-将视频抓拍到的人脸图像，与存有的人脸图像进行比对。
-
-![image](https://github.com/colanicy/face-compare/blob/master/facecompare.gif)
-
-## 运行环境：
+# 运行环境：
 
 opencv 版本opencv_python-3.1.0-cp27-cp27m-win32
 
@@ -14,20 +8,43 @@ python 版本 Python 2.7.13
 
 摄像头
 
-## 准备工作
+# 准备工作
 
 需要先到face++申请一个key，将main.py中的API_KEY和API_SECRET修改为自己的
 API_KEY = "<<your key>>"
 API_SECRET = "<<your secret>>"
 
+# First人脸比对测试 main_face
+
+将视频抓拍到的人脸图像，与存有的人脸图像进行比对。
+
+![image](https://github.com/colanicy/face-compare/blob/master/facecompare.gif)
+
+
+
 ## 操作
 
-运行命令：cd到main文件所在目录
+运行命令：cd到main_face文件所在目录
 
-然后执行>python main.py
+然后执行>python main_face.py
 
 最后屏幕输出比对结果。
 
 退出循环请按Q
 
 注意：比对的目标文件是source_pic/test.jpg,你可以把这个文件更换为你自己的照片进行比对。
+
+# First手势识别测试 main_gesture.py
+
+将视频抓怕到的手势进行识别，并指出其含义。可用于基于手势的人机交互。
+![image](https://github.com/colanicy/face-compare/blob/master/gesture2.gif)
+
+##操作
+运行命令：cd到main_face文件所在目录
+
+然后执行>python main_gesture.py
+
+最后屏幕输出识别结果。
+
+按任意键退出。
+
---
+++ main_gesture.py
@@ -0,0 +1,111 @@
+#-*-coding:utf-8-*-
+
+#通过Opencv获取手势，然后识别手势
+
+import cv2
+import sys
+import time
+from Lib.humanbodypp import API,File
+
+camID = 0
+
+capture = cv2.VideoCapture(camID)
+cv2.namedWindow("camera",1)
+
+if not capture.isOpened():
+	print("Camera open failed!")
+	print("Please inspect your camera deviceID %d."%camID)
+	sys.exit()
+
+#set FRAME_WIDTH,FRAME_HEIGHT
+'''
+设置摄像机分辨率的问题。
+最小分辨率，宽度为4，高度为3。
+以此类推应该为4的倍数或3的倍数。
+你可以通过videocapture成员函数set来设置，摄像机的分辨率。
+videocapture默认的情况下为640×480。
+'''
+FRAME_WIDTH = 640
+FRAME_HEIGHT = 480
+if capture.set(3,FRAME_WIDTH) and capture.set(4,FRAME_HEIGHT):
+	print "Frame resize successful!"
+else:
+	print "Frame resize failed!"
+
+class Rectangle:
+	topLeft=(326,0)
+	downRight=(640,350)
+	color=(170,170,0)
+	thickness=3
+	lineType=7
+
+	def __init__(self,topLeft = (0,0),downRight = (0,0),bgrcolor = (0,0,255),thickness = 3,lineType = 8):
+		if topLeft :
+			self.topLeft = topLeft
+		if downRight:
+			self.downRight = downRight
+		if bgrcolor:
+			self.color = bgrcolor
+		if thickness:
+			self.thickness = thickness
+		if lineType:
+			self.lineType = lineType
+	def area(self):
+		return (self.downRight[0] - self.topLeft[0]) * (self.downRight[1] - self.topLeft[1])
+
+start_time = time.time()
+while(True):
+	ha,img = capture.read()
+	end_time = time.time()
+	rec = Rectangle((326,0),(640,350),(170,170,0),3,8)
+	cv2.rectangle(img,rec.topLeft,rec.downRight,
+		rec.color,rec.thickness,rec.lineType)
+	cv2.putText(img,str(int((5-(end_time- start_time)))), (100,100), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+	cv2.imshow("camera",img)
+	if(end_time-start_time>5):
+            break
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+ha,img = capture.read()
+capture.release()
+gestureImg = img[0:300,326:640]
+
+cv2.imwrite("wif.jpg",gestureImg)
+cv2.namedWindow("gesture",cv2.WINDOW_NORMAL)
+
+#手势识别
+key = "MT8PUbZ-OiWlYNSa4Ge72SZxua9jOpS5"
+secret = "aK9SstvkPtfeEJPScLASLePc7DaCeGTt"
+api = API(key, secret)
+
+gesture = api.gesture(image_file = File("wif.jpg"))
+
+#根据识别结果，将识别框画到图片上，并进行显示
+if len(gesture["hands"])>0:
+	print "Detected valid gesture!"
+else:
+	print "Detecte failed! %d"%len(gesture["hands"])
+	sys.exit()
+rec_areas = []
+for  i in xrange(0,len(gesture["hands"])):
+	hand_rectangle = gesture["hands"][i]["hand_rectangle"]
+	rec = Rectangle((hand_rectangle["left"],hand_rectangle["top"]),
+		(hand_rectangle["left"]+hand_rectangle["width"],
+		hand_rectangle["top"]+hand_rectangle["height"]),
+		(170,170,0), 2, 5)
+
+	rec_areas.append(rec.area())
+
+	cv2.rectangle(gestureImg,rec.topLeft,rec.downRight,
+		rec.color,rec.thickness,rec.lineType)
+	
+cv2.imshow("gesture",gestureImg)
+cv2.imwrite("wif.jpg",gestureImg)
+#挑选面积最大的矩形
+recid = rec_areas.index(max(rec_areas))
+print max(gesture["hands"][recid]["gesture"],key=gesture["hands"][recid]["gesture"].get)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
